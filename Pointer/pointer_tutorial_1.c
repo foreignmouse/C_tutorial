896f6807e791b97a7ec607527baf32ef25d4b90a
Yang Jia <zju_yj@hotmail.com>
2021-07-20 17:42:51 +0800

    tidy pointer tutorials

--- Pointer/pointer_tutorial_1.c
+++ Pointer/pointer_tutorial_1.c
@@ -4,10 +4,17 @@ int main()
     int age;
     age = 10; 
     int *p;
-    // age = 10;
     p = &age;
-    printf("age: %d\n",age);
-    // printf(p);
-    printf("age variable address is %d\n",&p);
+    int **p2;
+    p2 = &p;
+    printf("age value: %d\n",age);
+    printf("age variable address: %d\n",p);
+    printf("verification, age variable pointer pointed value: %d\n",*p);
+    printf("age variable pointer address is %d\n",&p);
+
+    printf("pointer p's value: %d\n",p);
+    printf("verification, pointer p2 pointed value: %d\n",*p2);
+    printf("verification, pointer p2 pointed pointer's pointed value: %d\n",**p2);
+    printf("pointer p2 address is %d\n",&p2);
     return 0;
 }
